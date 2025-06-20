51ef4ddad35aac094e8c9d6461587a4051457506
Akshay Hebbar <akshayhebbar568@gmail.com>
2025-06-20 15:02:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React,{useState,useEffect, use} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import api from './api';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ const [todos, setTodos] = useState([]);
+useEffect(() => {
+  api.get('/')
+    .then(response => {
+      setTodos(response.data);
+    })
+    .catch(error => {
+      console.error('Error fetching todos:', error);
+    });
+}
+
+, []);
+return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Todo List</h1>
+        </header>
+        <main>
+          <Switch>
+            <Route path="/" exact>
+              <ul>
+                {todos.map(todo => (
+                  <li key={todo.id}>{todo.title}-{todo.completed?'Done':'Unfinished'}</li>
+                ))}
+              </ul>
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
