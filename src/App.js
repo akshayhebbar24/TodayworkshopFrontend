d2a6d7b4c22d2247fde6d9b4c93be6f5137aca53
Akshay Hebbar <akshayhebbar568@gmail.com>
2025-06-20 15:53:47 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,41 @@ import './App.css';
 import React,{useState,useEffect, use} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import api from './api';
+import TodoForm from './components/TodoForm';
+import ToList from './components/ToList';
+
 function App() {
  const [todos, setTodos] = useState([]);
+
+ const fetchTodos = () => {
+  api.get('/')
+    .then(response => {
+      setTodos(response.data);
+    })
+    .catch(error => {
+      console.error('Error fetching todos:', error);
+    });
+ }
+const deleteTodo = async(id) => {
+  try {
+    await api.delete(`/${id}`);
+    setTodos(todos.filter(todo => todo.id !== id));
+  } catch (error) {
+    console.error('Error deleting todo:', error);
+  }
+}
+
+const toggleTodo = async (id, completed) => {
+  try {
+    const updatedTodo = await api.put(`/${id}`, { completed: !completed });
+    setTodos(todos.map(todo => (todo.id === id ? updatedTodo.data : todo)));
+  } catch (error) {
+    console.error('Error toggling todo:', error);
+  }
+}
+
+
+
 useEffect(() => {
   api.get('/')
     .then(response => {
@@ -19,24 +52,14 @@ useEffect(() => {
 return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Todo List</h1>
-        </header>
-        <main>
-          <Switch>
-            <Route path="/" exact>
-              <ul>
-                {todos.map(todo => (
-                  <li key={todo.id}>{todo.title}-{todo.completed?'Done':'Unfinished'}</li>
-                ))}
-              </ul>
-            </Route>
-          </Switch>
-        </main>
+       <h1>To Do List</h1>
+        <TodoForm onTodoAdded={fetchTodos} />
+        <ToList todos={todos} onTodoDelete={deleteTodo} onTodoToggle={toggleTodo} />
+
       </div>
     </Router>
   );
 }
 
 export default App;
+
